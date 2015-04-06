a3bf0180b55ec9608b1bda7183030de0e4757d46
Felix Lu <f33losopher@gmail.com>
2015-04-05 22:02:15 -0700

    More updates to use GoogleTest

--- CrackingTheCodingInterview/TestMain.cpp
+++ CrackingTheCodingInterview/TestMain.cpp
@@ -344,109 +344,125 @@ TEST(Ch1StringsAndArrays, prob1_4) {
 
 	// END CH8
 
-	// Ch 19 Moderate
-//	int x = 5;
-//	int y = 33;
-//	Moderate::prob19_1(x,y);
+TEST(Ch19_Moderate, prob19_1) {
+	int x = 5;
+	int y = 33;
 
-	// Problem 19.2
-//	ticTacToe** grid = new ticTacToe*[3];
-//	for (int i=0; i < 3; ++i) {
-//		grid[i] = new ticTacToe[3];
-//	}
-//	clearTTTgrid(grid);
-//
-//	// Situation 1
-//	// x _ _
-//	// o o _
-//	// x _ x
-//	clearTTTgrid(grid);
-//	grid[0][0] = Moderate::X;
-//	grid[2][0] = Moderate::X;
-//	grid[2][2] = Moderate::X;
-//	grid[1][0] = Moderate::O;
-//	grid[1][1] = Moderate::O;
+	Moderate::prob19_1(x,y);
+	EXPECT_EQ(x, 33);
+	EXPECT_EQ(y, 5);
+}
+
+TEST(Ch19_Moderate, prob19_2) {
+	ticTacToe** grid = new ticTacToe*[3];
+	for (int i=0; i < 3; ++i) {
+		grid[i] = new ticTacToe[3];
+	}
+	clearTTTgrid(grid);
+
+	// Situation 1
+	// x _ _
+	// o o _
+	// x _ x
+	clearTTTgrid(grid);
+	grid[0][0] = Moderate::X;
+	grid[2][0] = Moderate::X;
+	grid[2][2] = Moderate::X;
+	grid[1][0] = Moderate::O;
+	grid[1][1] = Moderate::O;
 //	printTTTGrid(grid);
-//	cout << "X wins: " << Moderate::prob19_2(grid, Moderate::X) << endl;
-//	cout << "O wins: " << Moderate::prob19_2(grid, Moderate::O) << endl << endl;
-//
-//	// Situation 2
-//	// x _ _
-//	// o o o
-//	// x _ x
-//	clearTTTgrid(grid);
-//	grid[0][0] = Moderate::X;
-//	grid[2][0] = Moderate::X;
-//	grid[2][2] = Moderate::X;
-//	grid[1][0] = Moderate::O;
-//	grid[1][1] = Moderate::O;
-//	grid[1][2] = Moderate::O;
+	EXPECT_EQ(false, Moderate::prob19_2(grid, Moderate::X));
+	EXPECT_EQ(false, Moderate::prob19_2(grid, Moderate::O));
+
+	// Situation 2
+	// x _ _
+	// o o o
+	// x _ x
+	clearTTTgrid(grid);
+	grid[0][0] = Moderate::X;
+	grid[2][0] = Moderate::X;
+	grid[2][2] = Moderate::X;
+	grid[1][0] = Moderate::O;
+	grid[1][1] = Moderate::O;
+	grid[1][2] = Moderate::O;
 //	printTTTGrid(grid);
-//	cout << "X wins: " << Moderate::prob19_2(grid, Moderate::X) << endl;
-//	cout << "O wins: " << Moderate::prob19_2(grid, Moderate::O) << endl << endl;
-//
-//	// Situation 3
-//	// x _ _
-//	// o x _
-//	// o _ x
-//	clearTTTgrid(grid);
-//	grid[0][0] = Moderate::X;
-//	grid[1][1] = Moderate::X;
-//	grid[2][2] = Moderate::X;
-//	grid[1][0] = Moderate::O;
-//	grid[2][0] = Moderate::O;
+	EXPECT_EQ(false, Moderate::prob19_2(grid, Moderate::X));
+	EXPECT_EQ(true, Moderate::prob19_2(grid, Moderate::O));
+
+	// Situation 3
+	// x _ _
+	// o x _
+	// o _ x
+	clearTTTgrid(grid);
+	grid[0][0] = Moderate::X;
+	grid[1][1] = Moderate::X;
+	grid[2][2] = Moderate::X;
+	grid[1][0] = Moderate::O;
+	grid[2][0] = Moderate::O;
 //	printTTTGrid(grid);
-//	cout << "X wins: " << Moderate::prob19_2(grid, Moderate::X) << endl;
-//	cout << "O wins: " << Moderate::prob19_2(grid, Moderate::O) << endl << endl;
-//
-//	// Situation 4
-//	// _ _ x
-//	// _ x _
-//	// x o o
-//	clearTTTgrid(grid);
-//	grid[0][2] = Moderate::X;
-//	grid[1][1] = Moderate::X;
-//	grid[2][0] = Moderate::X;
-//	grid[2][1] = Moderate::O;
-//	grid[2][2] = Moderate::O;
+	EXPECT_EQ(true, Moderate::prob19_2(grid, Moderate::X));
+	EXPECT_EQ(false, Moderate::prob19_2(grid, Moderate::O));
+
+	// Situation 4
+	// _ _ x
+	// _ x _
+	// x o o
+	clearTTTgrid(grid);
+	grid[0][2] = Moderate::X;
+	grid[1][1] = Moderate::X;
+	grid[2][0] = Moderate::X;
+	grid[2][1] = Moderate::O;
+	grid[2][2] = Moderate::O;
 //	printTTTGrid(grid);
-//	cout << "X wins: " << Moderate::prob19_2(grid, Moderate::X) << endl;
-//	cout << "O wins: " << Moderate::prob19_2(grid, Moderate::O) << endl << endl;
+	EXPECT_EQ(true, Moderate::prob19_2(grid, Moderate::X));
+	EXPECT_EQ(false, Moderate::prob19_2(grid, Moderate::O));
+}
 
-	// Prob 19.3
-//	for(int i=0; i < 126; ++i) {
-//		cout << i << "  Num  trail zeros: " << Moderate::prob19_3(i) << endl;
-//	}
-	// END Prob 19.3
+TEST(Ch19_Moderate, prob19_3) {
+	EXPECT_EQ(Moderate::prob19_3(4), 0);
+	EXPECT_EQ(Moderate::prob19_3(5), 1);
+	EXPECT_EQ(Moderate::prob19_3(9), 1);
+	EXPECT_EQ(Moderate::prob19_3(10), 2);
+	EXPECT_EQ(Moderate::prob19_3(15), 3);
+	EXPECT_EQ(Moderate::prob19_3(20), 4);
+	EXPECT_EQ(Moderate::prob19_3(25), 6);
+	EXPECT_EQ(Moderate::prob19_3(50), 12);
+	EXPECT_EQ(Moderate::prob19_3(125), 31);
+}
 
-	// Prob 19.4
-//	cout << "Max of 3,4 = " << Moderate::prob19_4(3,4) << endl;
-//	cout << "Max of 10,20 = " << Moderate::prob19_4(10,20) << endl;
-//	cout << "Max of 10,10 = " << Moderate::prob19_4(10,10) << endl;
-//	cout << "Max of -1,1 = " << Moderate::prob19_4(-1,1) << endl;
-//	cout << "Max of -12,-18 = " << Moderate::prob19_4(-12,-18) << endl;
-
-	// END Prob 19.4
-
+TEST(Ch19_Moderate, prob19_4) {
+	EXPECT_EQ(Moderate::prob19_4(3,4), 4);
+	EXPECT_EQ(Moderate::prob19_4(10,20), 20);
+	EXPECT_EQ(Moderate::prob19_4(10,10), 10);
+	EXPECT_EQ(Moderate::prob19_4(-1,1), 1);
+	EXPECT_EQ(Moderate::prob19_4(-12,-18), -12);
+}
 
 
-	// Prob 19.5
-//	int hits;
-//	int pseudoHits;
-//	Moderate::prob19_5("RGGB", "YRGB", hits, pseudoHits);
-//	cout << "Hits: " << hits << "  PseudoHits: " << pseudoHits << endl;
-//	Moderate::prob19_5("BRGB", "YRGB", hits, pseudoHits);
-//	cout << "Hits: " << hits << "  PseudoHits: " << pseudoHits << endl;
-//	Moderate::prob19_5("YRGB", "YRGB", hits, pseudoHits);
-//	cout << "Hits: " << hits << "  PseudoHits: " << pseudoHits << endl;
-//	Moderate::prob19_5("RRGG", "BBYY", hits, pseudoHits);
-//	cout << "Hits: " << hits << "  PseudoHits: " << pseudoHits << endl;
-//	Moderate::prob19_5("RGYB", "BYGR", hits, pseudoHits);
-//	cout << "Hits: " << hits << "  PseudoHits: " << pseudoHits << endl;
-//	Moderate::prob19_5("RRRR", "BYGR", hits, pseudoHits);
-//	cout << "Hits: " << hits << "  PseudoHits: " << pseudoHits << endl;
+TEST(Ch19_Moderate, prob19_5) {
+	int hits;
+	int pseudoHits;
+	Moderate::prob19_5("RGGB", "YRGB", hits, pseudoHits);
+	EXPECT_EQ(hits, 2);
+	EXPECT_EQ(pseudoHits, 1);
 
-	// END Prob 19.5
+	Moderate::prob19_5("BRGB", "YRGB", hits, pseudoHits);
+	EXPECT_EQ(hits, 3);
+	EXPECT_EQ(pseudoHits, 0);
 
-	// END CH 19
+	Moderate::prob19_5("YRGB", "YRGB", hits, pseudoHits);
+	EXPECT_EQ(hits, 4);
+	EXPECT_EQ(pseudoHits, 0);
 
+	Moderate::prob19_5("RRGG", "BBYY", hits, pseudoHits);
+	EXPECT_EQ(hits, 0);
+	EXPECT_EQ(pseudoHits, 0);
+
+	Moderate::prob19_5("RGYB", "BYGR", hits, pseudoHits);
+	EXPECT_EQ(hits, 0);
+	EXPECT_EQ(pseudoHits, 4);
+
+	Moderate::prob19_5("RRRR", "BYGR", hits, pseudoHits);
+	EXPECT_EQ(hits, 1);
+	EXPECT_EQ(pseudoHits, 0);
+}
