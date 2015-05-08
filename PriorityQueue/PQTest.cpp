9837a588ca21aa8bddc085078cc50fd8e415194b
Felix Lu <f33losopher@gmail.com>
2015-05-07 23:22:25 -0700

    Priority Queue 2D sort

--- PriorityQueue/PQTest.cpp
+++ PriorityQueue/PQTest.cpp
@@ -10,10 +10,32 @@
 #include <vector>
 using namespace std;
 
+struct Point {
+	Point(int x, int y) : _x(x), _y(y) {
+	}
+	int _x;
+	int _y;
+
+	friend ostream& operator<<(ostream& o, const Point& p) {
+		cout << "x: " << p._x << " y: " << p._y << endl;
+		return o;
+	}
+};
+
 struct cmpChar {
 	bool operator() (const char a, const char b) {
 		// Return true if a is ordered before b
 		return a < b;
+	}
+};
+
+struct cmpPt {
+	bool operator() (const Point& p1, const Point& p2) {
+		if(p1._x == p2._x) {
+			return p1._y > p2._y;
+		} else {
+			return p1._x > p2._x;
+		}
 	}
 };
 
@@ -32,7 +54,23 @@ int main() {
 		cout << pq.top() << " ";
 		pq.pop();
 	}
-	cout << endl;
+	cout << endl << endl;
+
+	priority_queue<Point, vector<Point>, cmpPt> pq2;
+	pq2.push(Point(3,6));
+	pq2.push(Point(2,8));
+	pq2.push(Point(7,5));
+	pq2.push(Point(7,4));
+	pq2.push(Point(7,7));
+	pq2.push(Point(5,7));
+	pq2.push(Point(1,8));
+	pq2.push(Point(1,10));
+	pq2.push(Point(3,1));
+	pq2.push(Point(4,10));
+	while(!pq2.empty()) {
+		cout << pq2.top();
+		pq2.pop();
+	}
 	return 0;
 }
 
